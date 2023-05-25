d593b7c30a5c31a053e757c61683ccbc1619b777
Jeongan-Kim <stor9592@naver.com>
2023-05-25 13:50:56 +0900

    과제 틀 만듦

---
+++ C0524/strCreate.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int myStrlen(char str);
+
+char myStrcpy(char str);
+
+int myStrcmp(int str,int str2);
+
+int myStrcat(int str, int str2);
+
+int main()
+{
+	char str[100];
+	char str2[10];
+
+	printf("원하는 문자열 입력: ");
+
+	scanf("%s", str);
+
+
+
+
+	return 0;
+}
+
+
